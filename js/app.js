205863aa51f6a4ed79611d6c43a3367e2719ae69
GamingWorld <hamzehham07@gmail.com>
2026-07-11 20:54:42 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -63,6 +63,21 @@ searchInput.addEventListener("keydown", (event) => {
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("click", (e) => {
+
+    e.stopPropagation();
+
     navLinks.classList.toggle("active");
+
+});
+
+document.addEventListener("click", (e) => {
+
+    if (
+        !navLinks.contains(e.target) &&
+        !menuToggle.contains(e.target)
+    ) {
+        navLinks.classList.remove("active");
+    }
+
 });
